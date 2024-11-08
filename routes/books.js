fafdbce81b823f3208edb7bc24c6b6421deb261a
tlp1905 <tlpal@iscte-iul.pt>
2024-11-08 11:32:23 +0000

    Update 1.1
    Ponto 1 e 2 Terminados com o link de teste

--- routes/books.js
+++ routes/books.js
@@ -4,10 +4,31 @@ import db from "../db/config.js";
 import { ObjectId } from "mongodb";
 const router = express.Router();
 // return first 50 documents from movies collection
+
+//Pode ser apagado
+
 router.get("/", async (req, res) => {
 let results = await db.collection('books').find({})
 .limit(50)
 .toArray();
 res.send(results).status(200);
 });
+
+
+//Até aqui
+
+//Ponto 1 a funcionar
+//Alterar o numero 2 para mudar cada página
+//http://localhost:3000/books?page=2
+router.get("/", async(req,res)=>{
+    let page = req.query.page;
+let results = await db.collection('books').find({}).
+skip((page*10)-10).limit(10)
+.toArray();
+
+res.send(results).status(200);
+
+
+});
+
 export default router;
--- routes/users.js
+++ routes/users.js
@@ -4,29 +4,41 @@ import db from "../db/config.js";
 import { ObjectId } from "mongodb";
 const router = express.Router();
 // return first 50 documents from movies collection
-router.get("/", async (req, res) => {
-let results = await db.collection('users').find({})
-.limit(50)
-.toArray();
-res.send(results).status(200);
-});
+// router.get("/", async (req, res) => {
+// let results = await db.collection('users').find({})
+// .limit(50)
+// .toArray();
+// res.send(results).status(200);
+// });
 
 // Pesquisar pelo Id, falta o top 3 livros 
-router.get("/:id", async (req, res) => {
-    try {
+// router.get("/:id", async (req, res) => {
+//     try {
         
-        let results = await db.collection('users').find({
-            _id: parseInt(req.params.id) 
-        }).toArray();
+//         let results = await db.collection('users').find({
+//             _id: parseInt(req.params.id) 
+//         }).toArray();
 
         
-        res.status(200).send(results);
-    } catch (error) {
+//         res.status(200).send(results);
+//     } catch (error) {
        
-        res.status(500).send({ error: "Erro na busca de dados" });
-    }
-});
+//         res.status(500).send({ error: "Erro na busca de dados" });
+//     }
+// });
 
+//Ponto 2
+//Teste:
+//http://localhost:3000/users?page=1
+router.get("/", async(req,res)=>{
+    let page = req.query.page;
+let results = await db.collection('users').find({}).
+skip((page*10)-10).limit(10)
+.toArray();
+
+res.send(results).status(200);
+
+});
 
 
 export default router;
